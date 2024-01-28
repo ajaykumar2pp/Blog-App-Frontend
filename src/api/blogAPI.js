a0bed11263916a2df75ad6a3e3285d43cedb479b
ajaykumar2pp <ajay2kumarpp@gmail.com>
2024-01-28 12:18:18 +0530

    blogAPI Change

--- src/api/blogAPI.js
+++ src/api/blogAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8500/",
+  // baseURL: "http://localhost:8500/",
+  baseURL: "https://blog-test-api-8ci6.onrender.com/",
 });
